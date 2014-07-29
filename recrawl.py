8e9bf5abd16045b8428e0eaa041db542e1d289a4
Paul Yuan <puyuan1@gmail.com>
2014-07-29 10:28:22 +0800

    add new recrawl files, and modifed extract.py to support mongodb

---
+++ recrawl.py
@@ -0,0 +1,62 @@
+from bs4 import BeautifulSoup
+from  wget import crawl
+import bsddb
+import sys
+import datetime
+from pymongo import MongoClient
+
+
+db=bsddb.btopen("crawledPages.db", "c")
+
+
+f_all=open("all.csv", "a")
+#f_undervalued=open("undervalued", "a")
+
+
+def extract(link):
+	html_doc=crawl(link)
+	soup=BeautifulSoup(html_doc, "lxml")
+	print soup.title.string
+	title=soup.title.string
+	stock=soup.find(id="about_primary_stocks").get_text()
+	article_body=soup.find(id="article_body").get_text()
+	timestamp=str(datetime.datetime.now())
+	#print article_body
+	undervalued=0
+	discount=0
+	if(article_body.find("undervalued")>=0 or title.lower().find("undervalued")>=0):
+		undervalued=1
+	if(article_body.find("discount")>=0):
+		discount=1
+        f_all.write('"%s","%s",%d,%d,"%s",%s\n'%(soup.title.string, stock, undervalued,discount, link, timestamp))
+	print "found undervalued instances: "+ str(article_body.find("undervalued"))
+	db[str(link)]='1'
+	record={ "title": soup.title.string,
+                  "stock": stock, 
+                  "link": link, 
+                   "timestamp": timestamp, 
+                  "content": article_body
+	
+       		 }
+	db_mongo["test_collection"].insert(record)
+  
+
+	
+
+
+
+
+# starting up Mongo Client
+client=MongoClient("localhost", 27017)
+db_mongo=client.test_database
+
+
+for url, flag in db.iteritems():
+	try:
+		extract(url)
+	except:
+		e = sys.exc_info()[0]
+		print( "<p>Error: %s</p>" % e )
+
+f_all.close()
+db.close()
